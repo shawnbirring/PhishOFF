921e47365272487aa79f464c024ae6a624739e79
scoobydoo <49530430+Claireko@users.noreply.github.com>
2025-04-02 18:03:10 -0700

    Update jest.config.js

--- extension/jest.config.js
+++ extension/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   testMatch: [
-    'extension/__tests__/**/*.test.js',
+    '**/extension/__tests__/**/*.test.js',  
   ],
 };
