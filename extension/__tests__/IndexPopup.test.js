25b74733586ef87b29806251fb022acd89dfe9a6
scoobydoo <49530430+Claireko@users.noreply.github.com>
2025-04-02 19:02:07 -0700

    Update IndexPopup.test.js

--- extension/__tests__/IndexPopup.test.js
+++ extension/__tests__/IndexPopup.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import IndexPopup from "./popup";
+import IndexPopup from "../popup";
 
 describe("IndexPopup Component", () => {
     test("renders the component correctly", () => {
