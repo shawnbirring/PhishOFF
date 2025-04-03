708a0d65ebafa3c03f47d3a05cc0486f40f66f31
scoobydoo <49530430+Claireko@users.noreply.github.com>
2025-04-02 18:59:28 -0700

    Update jest.config.js

--- extension/jest.config.js
+++ extension/jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'], // Add 'ts' and 'tsx' here
   testMatch: [
-    '**/extension/__tests__/**/*.test.js',    
+    '**/extension/__tests__/**/*.test.js',
   ],
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest', // Use ts-jest to handle TypeScript files (.ts and .tsx)
+    '^.+\\.jsx?$': 'babel-jest', // Keep babel-jest for JavaScript and JSX
   },
 };
-
