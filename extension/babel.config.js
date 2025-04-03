27959a6eccc2493d33e373a45a676ffe36f3ee83
scoobydoo <49530430+Claireko@users.noreply.github.com>
2025-04-02 18:21:13 -0700

    Update babel.config.js

--- extension/babel.config.js
+++ extension/babel.config.js
@@ -2,5 +2,6 @@ module.exports = {
   presets: [
     '@babel/preset-env',    // Handles modern JavaScript
     '@babel/preset-react',  // Handles JSX
+    '@babel/preset-typescript', // Handles TypeScript
   ],
 };
