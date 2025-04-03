50d2bd39e56e008c2213ca77af2a39b89a97afbc
scoobydoo <49530430+Claireko@users.noreply.github.com>
2025-04-02 17:27:39 -0700

    Create jest.config.js

---
+++ extension/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testMatch: [
+    '**/tests/**/*.test.js',  // Ensure your test files are picked up
+  ],
+};
